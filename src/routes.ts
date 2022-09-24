9f4ca8e4ba4a0ef29b0cea183ceacde7fa3850de
Gabrielcefetzada <gabrielaugustosb13@gmail.com>
2022-09-24 16:30:09 -0300

    feat(compliments): create compliments route

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,18 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController";
 import { AuthController } from "./controllers/AuthController";
+import { CreateComplimentController } from "./controllers/ComplimentsController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", createTagController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("compliment", createComplimentController.handle);
 
 export { router };
