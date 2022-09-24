e3e91346a873b26b6a1a88a553962ecf67087dd3
Gabrielcefetzada <gabrielaugustosb13@gmail.com>
2022-09-24 16:23:51 -0300

    refactor(compliments): ident service

--- src/services/ComplimentService.ts
+++ src/services/ComplimentService.ts
@@ -1,8 +1,6 @@
-
-
-import { getCustomRepository } from "typeorm"
-import { ComplimentsRespositories } from "../repositories/ComplimentRepositories"
-import { UsersRepositories } from "../repositories/UserRepositories"
+import { getCustomRepository } from "typeorm";
+import { ComplimentsRespositories } from "../repositories/ComplimentRepositories";
+import { UsersRepositories } from "../repositories/UserRepositories";
 
 interface ComplimentsRequestInterface {
     tag_id: string;
@@ -12,31 +10,39 @@ interface ComplimentsRequestInterface {
 }
 
 class CreateComplimentService {
-    async execute({ tag_id, user_sender, user_receiver, message} : ComplimentsRequestInterface){
-        const complimentsRespositories = getCustomRepository(ComplimentsRespositories)
+    async execute({
+        tag_id,
+        user_sender,
+        user_receiver,
+        message,
+    }: ComplimentsRequestInterface) {
+        const complimentsRespositories = getCustomRepository(
+            ComplimentsRespositories
+        );
 
-        const userRepositories = getCustomRepository(UsersRepositories)
+        const userRepositories = getCustomRepository(UsersRepositories);
 
-        if(user_receiver === user_sender){
-            throw new Error("The users can't send to theirself")
+        if (user_receiver === user_sender) {
+            throw new Error("The users can't send to theirself");
         }
-        const userReceiverExists = await userRepositories.findOne({id: user_receiver})
+        const userReceiverExists = await userRepositories.findOne({
+            id: user_receiver,
+        });
 
-        if(!userReceiverExists) {
-            throw new Error("User receiver does not exist")
+        if (!userReceiverExists) {
+            throw new Error("User receiver does not exist");
         }
 
         const compliment = complimentsRespositories.create({
             tag_id,
             user_receiver,
             user_sender,
-            message
-        })
+            message,
+        });
 
-        await complimentsRespositories.save(compliment)
-        return compliment
-
+        await complimentsRespositories.save(compliment);
+        return compliment;
     }
 }
 
-export { CreateComplimentService }
+export { CreateComplimentService };
